8a3ed19fd38a726865278702fafcf5b24070879a
Gorazd Štieber <58399636+gstibi@users.noreply.github.com>
2022-11-20 00:17:20 +0100

    header-updated-to-carousel (#17)

--- src/components/home/ButtonsSection.jsx
+++ src/components/home/ButtonsSection.jsx
@@ -8,43 +8,36 @@ const pages = getRoutesAndIds();
 
 const useStyles = makeStyles(() => ({
   link: { textDecoration: 'none' },
+  button: { width: 'max-content' },
 }));
 
 const ButtonsSection = () => {
   const classes = useStyles();
   return (
-    <Grid
-      container
-      spacing={1}
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        justifySelf: 'stretch',
-        display: 'flex',
-        marginTop: '1%',
-        marginBottom: '1%',
-        padding: '1% 2%',
-      }}
-    >
-      <Grid item xs={12} sm={3}>
-        <Link
-          to="/"
-          className={classes.link}
-          onClick={() => scrollToElement(pages[3].id)}
-        >
-          <Button variant="contained" fullWidth>
-            {pages[3].name}
-          </Button>
-        </Link>
-      </Grid>
-      <Grid item xs={12} sm={3}>
+    <Grid container spacing={1}>
+      <Grid item xs={12} sm={6} display="flex" justifyContent="center">
         <Link
           to="/"
           className={classes.link}
           onClick={() => scrollToElement(pages[1].id)}
         >
-          <Button color="secondary" variant="contained" fullWidth>
+          <Button variant="contained" className={classes.button}>
             Viac informácií
+          </Button>
+        </Link>
+      </Grid>
+      <Grid item xs={12} sm={6} display="flex" justifyContent="center">
+        <Link
+          to="/"
+          className={classes.link}
+          onClick={() => scrollToElement(pages[3].id)}
+        >
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+          >
+            {pages[3].name}
           </Button>
         </Link>
       </Grid>
